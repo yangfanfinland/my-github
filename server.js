6a735ec119cde2845202756c92ea4c1f40f972e9
yangfanfinland <yangfanfinland@gmail.com>
2020-10-28 23:15:22 +0200

    redis connection test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const next = require('next')
 
 const dev = process.env.NODE_ENV === 'production'
@@ -7,11 +8,22 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
     const server = new Koa();
+    const router = new Router()
+
+    router.get('/test', (ctx) => {
+        ctx.body = '<span>request / test</span>'
+    })
+
+    // server.use(async (ctx, next) => {
+    //     await handle(ctx.req, ctx.res)
+    //     ctx.respond = false
+    // })
 
     server.use(async (ctx, next) => {
-        await handle(ctx.req, ctx.res)
-        ctx.respond = false
+        await next()
     })
+
+    server.use(router.routes())
 
     server.listen(3000, () => {
         console.log('koa server listening on port 3000')
