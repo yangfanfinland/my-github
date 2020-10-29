863e27c3f43cdfdd4ae7879c6c2d80ef84373065
yangfanfinland <yangfanfinland@gmail.com>
2020-10-29 15:17:42 +0200

    Integrate styled-component

--- pages/test/a.js
+++ pages/test/a.js
@@ -1,17 +1,38 @@
+import Link from 'next/link'
+import styled from 'styled-components' 
 import { withRouter } from 'next/router'
-import Comp from '../../components/comp'
 
-const A = ({ router, name }) => <Comp>A { router.query.id } {name}</Comp>
+const Title = styled.h1 `
+    color: yellow;
+    font-size: 40px;
+`
+
+
+const A = ({ router, name }) => (
+  <>
+    <Title>This is title</Title>
+    <Link href="#aaa">
+      <a>
+        A {router.query.id} {name}
+      </a>
+    </Link>
+    <style jsx>{`
+        a {
+            color: blue
+        }
+    `}</style>
+  </>
+)
 
 A.getInitialProps = async () => {
-    const promise = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                name: 'fan'
-            })
-        }, 1000)
-    })
-    return await promise
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'fan',
+      })
+    }, 1000)
+  })
+  return await promise
 }
 
 export default withRouter(A)
