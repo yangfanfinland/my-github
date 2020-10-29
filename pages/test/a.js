c40d34cd8758d5e53d983ee248e833bce91a1fd7
yangfanfinland <yangfanfinland@gmail.com>
2020-10-29 16:23:50 +0200

    Config options

--- pages/test/a.js
+++ pages/test/a.js
@@ -1,34 +1,47 @@
-import Link from 'next/link'
-import styled from 'styled-components' 
 import { withRouter } from 'next/router'
+import getConfig from 'next/config'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+import styled from 'styled-components'
 
-const Title = styled.h1 `
-    color: yellow;
-    font-size: 40px;
+const Comp = dynamic(import('../../components/comp'))
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+const Title = styled.h1`
+  color: yellow;
+  font-size: 40px;
 `
 
+const A = ({ router, name, time }) => {
+  console.log(serverRuntimeConfig, publicRuntimeConfig)
 
-const A = ({ router, name }) => (
-  <>
-    <Title>This is title</Title>
-    <Link href="#aaa">
-      <a>
-        A {router.query.id} {name}
-      </a>
-    </Link>
-    <style jsx>{`
+  return (
+    <>
+      <Title>This is title {time}</Title>
+      <Comp />
+      <Link href="#aaa">
+        <a>
+          A {router.query.id} {name} {process.env.customKey}
+        </a>
+      </Link>
+      <style jsx>{`
         a {
-            color: blue
+          color: blue;
         }
-    `}</style>
-  </>
-)
+      `}</style>
+    </>
+  )
+}
 
-A.getInitialProps = async () => {
+A.getInitialProps = async (ctx) => {
+  const moment = await import('moment')
+
   const promise = new Promise((resolve) => {
     setTimeout(() => {
       resolve({
         name: 'fan',
+        time: moment.default(Date.now() - 60 * 1000).fromNow(),
       })
     }, 1000)
   })
