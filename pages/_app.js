78aa1a034d8bf206c03159d4725a57658a1077d9
yangfanfinland <yangfanfinland@gmail.com>
2020-10-30 14:09:33 +0200

    Add loading globally when switching pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,42 @@
 import App from 'next/app'
 import { Provider } from 'react-redux'
 import Layout from '../components/Layout'
-
+import PageLoading from '../components/PageLoading'
 import withRedux from '../lib/with-redux'
-
+import Router from 'next/router'
+import Link from 'next/link'
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
 
 class MyApp extends App {
+  state = {
+    loading: false
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true
+    })
+  }
+
+  stopLoading = () => {
+    this.setState({
+      loading: false
+    })
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading)
+    Router.events.on('routeChangeComplete', this.stopLoading)
+    Router.events.on('routeChangeError',this.stopLoading)
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading)
+    Router.events.off('routeChangeComplete', this.stopLoading)
+    Router.events.off('routeChangeError',this.stopLoading)
+  }
+
   static async getInitialProps(ctx) {
     const { Component } = ctx
     let pageProps = {}
@@ -24,7 +53,14 @@ class MyApp extends App {
 
     return (
       <Provider store={reduxStore}>
+        { this.state.loading ? <PageLoading /> : null }
         <Layout>
+          <Link href="/">
+            <a>Index</a>
+          </Link>
+          <Link href="/detail">
+            <a>Detail</a>
+          </Link>
           <Component {...pageProps} />
         </Layout>
       </Provider>
--- pages/detail.js
+++ pages/detail.js
@@ -1,5 +1,15 @@
-export default () => {
+function Detail() {
     return (
         <span>Detail</span>
     )
 }
+
+Detail.getInitialProps = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({})
+        }, 1000)
+    })
+}
+
+export default Detail
