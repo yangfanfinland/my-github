634e9c780209468730da6de1e3fb381523a17a8c
yangfanfinland <yangfanfinland@gmail.com>
2020-11-01 21:41:36 +0200

    bugfix: Readme.md could be missing & add atob as node global

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Production ENV start
+
+```bash
+pm2 start
+```
+
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Router = require('koa-router')
 const next = require('next')
 const session = require('koa-session')
 const koaBody = require('koa-body')
+const atob = require('atob')
 const RedisSessionStore = require('./server/session-store')
 const Redis = require('ioredis')
 const auth = require('./server/auth')
@@ -12,6 +13,9 @@ const dev = process.env.NODE_ENV === 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const redis = new Redis()
+
+// Add atob as node global
+global.atob = atob
 
 app.prepare().then(() => {
   const server = new Koa()
